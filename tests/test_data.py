001b7f307fa6410e6ee4c00a2b6b7d630e9cb89a
Daniel Svendsen <d.h.svendsen@gmail.com>
2023-01-09 15:18:03 +0100

    changed data

--- tests/test_data.py
+++ tests/test_data.py
@@ -10,10 +10,10 @@ def test_len():
     train_images = torch.load(train_image_path)
     train_labels = torch.load(f"{_PATH_DATA}/processed/train_labels.pt")
     # Number
-    assert len(train_images) == 25000, "Dataset did not have the correct number of samples"
+    assert len(train_images) == 40000, "Dataset did not have the correct number of samples"
     # Shape
     assert train_images.shape[1] == 28, "Wrong image height"
     assert train_images.shape[2] == 28, "Wrong image width"
     # Labels
-    assert len(train_labels) == 25000, "Labels did not have the correct number of samples"
+    assert len(train_labels) == 40000, "Labels did not have the correct number of samples"
     assert np.array_equiv( np.unique(train_labels.numpy()), np.array([0,1,2,3,4,5,6,7,8,9]) ), "Not all labels are represented"
